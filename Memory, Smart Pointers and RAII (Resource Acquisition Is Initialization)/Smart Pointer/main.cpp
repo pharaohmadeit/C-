d592001c7d95058d8582d293607b48fc8c369516
Pharaoh Tornes <155219985+pharaohmadeit@users.noreply.github.com>
2025-01-22 11:49:55 -0800

    Add files via upload
    Managing dynamic memory with smart pointers

---
+++ Memory, Smart Pointers and RAII (Resource Acquisition Is Initialization)/Smart Pointer/main.cpp
@@ -0,0 +1,41 @@
+//
+//  main.cpp
+//  fig11_02
+//
+//  Created by Pharaoh Tornes on 1/22/25.
+//
+#include <memory>
+#include <iostream>
+
+using namespace std;
+
+class Integer {
+
+public:
+    //  Constructor
+    Integer(int i) : value{i} {
+        cout << "Constructor for Integer " << value << "\n";
+    }
+    
+    //  Destructor
+    ~Integer() {
+        cout << "\nDestructor for Integer " << value << "\n";
+    }
+    
+    //  Integer value getter
+    int getValue() const {return value;}
+    
+private:
+    int value{0};
+};
+
+//  use unique_ptr to manipulate Integer object
+int main() {
+    cout << "Creating a unique_ptr object that points to an Integer\n\n";
+    
+    //  "aim" unique_ptr object at a new Integer object
+    auto ptr{make_unique<Integer>(7)};
+    
+    //  use unique_ptr to call Integer member function
+    cout << "\nInteger value: " << ptr->getValue() << "\n\nmain() ends (ptr goes out of scope and calls object's destructor)\n";
+}
