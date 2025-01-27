31b2c7a03f0bbbc76adae041cc2737cc0ef402ea
Pharaoh Tornes <155219985+pharaohmadeit@users.noreply.github.com>
2025-01-26 20:48:14 -0800

    Update main.cpp

--- Exceptions and Contracts/bad_alloc/main.cpp
+++ Exceptions and Contracts/bad_alloc/main.cpp
@@ -1,4 +1,5 @@
-//  Demonstrate standard "new" throwing bad_alloc when memory cannot be allocated. Stuff an array of 1000 pointers each aimed at an array of 500,000,000 doubles
+//  Demonstrate standard "new" throwing bad_alloc when memory cannot be allocated. 
+//  Initialize an array of 1000 pointers each aimed at an array of 500,000,000 doubles
 //  main.cpp
 //  fig12_06
 //
