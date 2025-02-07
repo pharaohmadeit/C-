1d7f45af7facc5daa13ff5e85e79cb3241aa72ae
Pharaoh Tornes <155219985+pharaohmadeit@users.noreply.github.com>
2025-02-07 08:46:13 -0800

    Update main.cpp

--- Containers and Iterators (Standard Library)/Vectors/Vector Iteration/main.cpp
+++ Containers and Iterators (Standard Library)/Vectors/Vector Iteration/main.cpp
@@ -17,24 +17,27 @@ void showResult(int value, size_t size, size_t capacity) {
 }
 
 int main() {
-    
+    //    create an empty vector of ints via call to default constructor
     vector<int> integers{};
     
     cout << "Size of integers: " << integers.size() << "\nCapacity of integers: " << integers.capacity() << "\n\n";
-    
+
+    //    append 1-10 to integers and display size and capacity
     for (int i : views::iota(1,11)) {
-        integers.push_back(i);  //  push_back member function is available for vector, deque and list
+        integers.push_back(i);  //  push_back member function is available for vector, deque and list (aka append)
         showResult(i, integers.size(), integers.capacity());
     }
     
     cout << "\nOutput integers using iterators: ";
-    
+
+    //    display vector using const_interator; initialize iterator, specify termination, preincrement constInterator
     for (auto constIterator{integers.cbegin()}; constIterator!= integers.cend(); ++constIterator) {
         cout << *constIterator << ' ';
     }
     
     cout << "\nOutput integers in reverse using iterators: ";
-    
+
+    //    display vector in reverse order using const_reverse_iterator
     for (auto reverseIterator{integers.crbegin()}; reverseIterator!= integers.crend(); ++reverseIterator) {
         cout << *reverseIterator << ' ';
     }
