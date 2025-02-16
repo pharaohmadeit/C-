a53e7d749f8319aef6c4fa10c90d88607e601f34
Pharaoh Tornes <155219985+pharaohmadeit@users.noreply.github.com>
2025-02-15 19:39:39 -0800

    Add files via upload

---
+++ Containers and Iterators (Standard Library)/Container Adaptors/Queue/main.cpp
@@ -0,0 +1,33 @@
+//  Queue adaptor (FIFO) first-in, first-out
+//  main.cpp
+//  fig13_11
+//
+//  Created by Pharaoh Tornes on 2/15/25.
+//
+#include <queue>
+#include <list>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // queue with doubles
+    queue<double> values{};
+    
+    //  push elements onto queue values
+    values.push(3.2);
+    values.push(9.8);
+    values.push(5.4);
+    
+    cout << "Popping from values: ";
+    
+    // pop elements from queue
+    while (!values.empty()) {
+        cout << values.front() << ' '; //  view front element
+        values.pop();   //  remove element
+    }
+    
+    cout << "\n";
+    
+}
