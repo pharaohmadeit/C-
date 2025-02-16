61b129f66f31f622fae0e2f30eff10b527c5e379
Pharaoh Tornes <155219985+pharaohmadeit@users.noreply.github.com>
2025-02-15 19:45:55 -0800

    Add files via upload

---
+++ Containers and Iterators (Standard Library)/Container Adaptors/Priority_Queue/main.cpp
@@ -0,0 +1,31 @@
+//  Standary libary priority_queue adaptor class
+//  main.cpp
+//  fig13_12
+//
+//  Created by Pharaoh Tornes on 2/15/25.
+//
+#include <queue>    //  priority_queue adaptor defintion
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // create priority_queue with doubles
+    priority_queue<double> priorities{};
+    
+    //  push elements onto priorities
+    priorities.push(3.2);
+    priorities.push(9.8);
+    priorities.push(5.4);
+    
+    cout << "Popping from values: ";
+    
+    // pop elements from priority_queue
+    while (!priorities.empty()) {
+        cout << priorities.top() << ' '; //  view top element
+        priorities.pop();   //  remove top element
+    }
+    
+    cout << "\n";
+    
+}
