3ba5169e81d966f5d0b2d2cc8870337c9d2923be
Pharaoh Tornes <155219985+pharaohmadeit@users.noreply.github.com>
2025-04-21 09:52:53 -0700

    Update main.cpp

--- Algorithms (Standard Library)/find, find_if, find_if_not, binary_search, all_of, any_of, none_of/main.cpp
+++ Algorithms (Standard Library)/find, find_if, find_if_not, binary_search, all_of, any_of, none_of/main.cpp
@@ -1,4 +1,4 @@
-//  C++20's Standard Template Library Algorithms: find, find_if, find_if_not, binary_search, all_of, any_of, none_of
+//  C++20's std::ranges Algorithms (Standard Template Library): find, find_if, find_if_not, binary_search, all_of, any_of, none_of
 //  main.cpp
 //  fig14_07
 //
