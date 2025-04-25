5c0757ca58f8f3c1b0607e868ff1e4807333323e
Pharaoh Tornes <pharaohmadeit@icloud.com>
2025-04-25 14:49:58 -0700

    Removing namespace std

--- Algorithms (Standard Library)/Lambda Expressions/main.cpp
+++ Algorithms (Standard Library)/Lambda Expressions/main.cpp
@@ -9,24 +9,22 @@
 #include <array>
 #include <iostream>
 
-using namespace std;
-
 int main() {
     //  initialize an array of ints using CTAD (class template argument deduction)
-    array values{1, 2, 3, 4};
-    ostream_iterator<int> output{cout, " "};
+    std::array values{1, 2, 3, 4};
+    std::ostream_iterator<int> output{std::cout, " "};
     
     // using ranges namespace copy algorithm to output the array to the standard output stream
-    cout << "values contains: ";
-    ranges::copy(values, output);
+    std::cout << "values contains: ";
+    std::ranges::copy(values, output);
     
     //  output each element multiplied by two; use for_each with a lambda expression
-    cout << "\nDisplay each element multiplied by two: ";
-    ranges::for_each(values, [](auto i) {cout << i * 2 << " ";});
+    std::cout << "\nDisplay each element multiplied by two: ";
+    std::ranges::for_each(values, [](auto i) {std::cout << i * 2 << " ";});
     
     //  add each element to sum; use lambda expression and capture the sum by reference in the introducer so it can be summed within the body of the lambda with the iterator in the parameter list
     int sum{0};
-    ranges::for_each(values, [&sum](auto i) {sum += i;});
-    cout << "\nSum of value's elements is: " << sum << "\n";
+    std::ranges::for_each(values, [&sum](auto i) {sum += i;});
+    std::cout << "\nSum of value's elements is: " << sum << "\n";
     
 }
