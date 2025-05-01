f4e816daff1dbdd25229dccd0e1220d0314b0117
Pharaoh Tornes <pharaohmadeit@icloud.com>
2025-04-30 18:18:45 -0700

    Demonstrating Filter, Map and Reduce using functional-style programming with internal iteration and the pipe syntax. Also removing bad code ("using namespace std").

--- Containers and Iterators (Standard Library)/Ranges and Views/main.cpp
+++ Containers and Iterators (Standard Library)/Ranges and Views/main.cpp
@@ -9,44 +9,43 @@
 #include <array>
 #include <format>
 #include <iostream>
-using namespace std;
 
 int main() {
     auto showValues {
-        [](auto& values, const string& message) {
-            cout << format("{}: ", message);
+        [](auto& values, const std::string& message) {
+            std::cout << std::format("{}: ", message);
             
             for (const auto& value : values) {
-                cout << format("{} ", value);
+                std::cout << std::format("{} ", value);
             }
             
-            cout << "\n\n";
+            std::cout << "\n\n";
         }
     };
     
-    //iota generates integers 1-10 using lazy programming, meaning it doesn't exist until it's called
-    auto values1{views::iota(1, 11)};
+    //  std::views::iota generates integers 1-10 using lazy programming, meaning it doesn't exist until it's called
+    auto values1{std::views::iota(1, 11)};
     showValues(values1, "Generate integers 1-10");
     
-    //filter
-    auto values2{values1 | views::filter([](const auto& x) {return x % 2 ==0;})};
+    //  filter using the pipeline operator; std::views::filter accepts a functions as an argument
+    auto values2{values1 | std::views::filter([](const auto& x) {return x % 2 ==0;})};
     showValues(values2, "Filtering even integers");
     
-    //map with views::transform
-    auto value3{values2 | views::transform([](const auto& x) {return x * x;})};
+    //  map with std::views::transform using the pipeline operator
+    auto value3{values2 | std::views::transform([](const auto& x) {return x * x;})};
     showValues(value3, "Mapping even integers to squares");
     
-    //filter & mapping with a pipeline operator |
-    auto values4{values1 | views::filter([](const auto& x) {return x % 2 ==0;}) | views::transform([](const auto& x) {return x * x;})};
+    //  filter & mapping with a pipeline operator in succession
+    auto values4 {values1 | std::views::filter([](const auto& x) {return x % 2 ==0;}) | std::views::transform([](const auto& x) {return x * x;})};
     showValues(values4, "Square of even integers");
     
-    //sum with numeric::accumulate
-    cout << format("Sum of squares of even integers 2-10: {}\n\n", accumulate(begin(values4), end(values4), 0));
+    //  sum with std::numeric::accumulate
+    std::cout << std::format("Sum of squares of even integers 2-10: {}\n\n", std::accumulate(std::begin(values4), std::end(values4), 0));
     
-    //filtering and mapping an existing container's elements; data isn't lazy but process pipeline is still lazy, meaning it doesn't process until it's called
-    constexpr array numbers{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    auto values5{numbers | views::filter([](const auto& x) {return x % 2 == 0;})
-                         | views::transform([](const auto& x){return x * x;})};
+    //  filtering and mapping an existing container's elements; data initialization isn't lazy but process pipeline is still lazy, meaning it doesn't process until it's called
+    constexpr std::array numbers{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    auto values5{numbers | std::views::filter([](const auto& x) {return x % 2 == 0;})
+                         | std::views::transform([](const auto& x){return x * x;})};
     showValues(values5, "Square of even integers in array numbers");
     
 }
