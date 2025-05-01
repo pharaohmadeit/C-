1a4f758cc05c88e368c2cc69e44272e8002362b2
Pharaoh Tornes <pharaohmadeit@icloud.com>
2025-04-30 18:45:05 -0700

    The ranges library includes range algorithms (i.e Filter, Map and Reduce; or std::views::filter, std::views::transform, std::accumulate) which are applied to ranges eagerly, and range adaptors, which are applied to views lazily. Adaptors can be composed into pipelines, so that their actions take place as the view is iterated. The "pipe" syntax of composing views takes the place of a traditional "functional" composing syntax. https://en.cppreference.com/w/cpp/ranges

--- Containers and Iterators (Standard Library)/Ranges and Views/main.cpp
+++ Containers and Iterators (Standard Library)/Ranges and Views/main.cpp
@@ -11,6 +11,7 @@
 #include <iostream>
 
 int main() {
+    //  create a lambda that accepts two arguments, one of numeric values and the other is a string message. use format to print out with std::cout
     auto showValues {
         [](auto& values, const std::string& message) {
             std::cout << std::format("{}: ", message);
@@ -27,22 +28,22 @@ int main() {
     auto values1{std::views::iota(1, 11)};
     showValues(values1, "Generate integers 1-10");
     
-    //  filter using the pipeline operator; std::views::filter accepts a functions as an argument
+    //  filter using the pipe operator; std::views::filter accepts a functions as an argument
     auto values2{values1 | std::views::filter([](const auto& x) {return x % 2 ==0;})};
     showValues(values2, "Filtering even integers");
     
-    //  map with std::views::transform using the pipeline operator
+    //  map with std::views::transform using the pipe operator
     auto value3{values2 | std::views::transform([](const auto& x) {return x * x;})};
     showValues(value3, "Mapping even integers to squares");
     
-    //  filter & mapping with a pipeline operator in succession
+    //  filter & mapping with the pipe operator in succession
     auto values4 {values1 | std::views::filter([](const auto& x) {return x % 2 ==0;}) | std::views::transform([](const auto& x) {return x * x;})};
     showValues(values4, "Square of even integers");
     
     //  sum with std::numeric::accumulate
     std::cout << std::format("Sum of squares of even integers 2-10: {}\n\n", std::accumulate(std::begin(values4), std::end(values4), 0));
     
-    //  filtering and mapping an existing container's elements; data initialization isn't lazy but process pipeline is still lazy, meaning it doesn't process until it's called
+    //  filtering and mapping an existing container's elements; data initialization isn't lazy but process pipeline is lazily evaluated, meaning it doesn't process until it's called
     constexpr std::array numbers{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     auto values5{numbers | std::views::filter([](const auto& x) {return x % 2 == 0;})
                          | std::views::transform([](const auto& x){return x * x;})};
